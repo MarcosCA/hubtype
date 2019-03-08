dba78bb5eddc00b8cf33dcdb5667f9ab728d543f
Marcos Calafat <marcos.calafat@boats.com>
2019-03-08 19:45:46 +0100

    dsasa

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,14 @@
 var searchController = require('../controllers/search');
 
 var appRouter = function(app) {
+
+    app.use((req, res, next) => {
+        res.append('Access-Control-Allow-Origin', ['*']);
+        res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.append('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+    });
+    
     app.get("/search", function(req, res) {
         searchController.search(req, res)
     });
